4f2d18dea16a73174f2b6c8742698a2891dedef3
Milen Dzhumerov <milen@thecosmicmachine.com>
2017-08-22 20:57:06 +0100

    Update to v1.0.2

--- Formula/hmap.rb
+++ Formula/hmap.rb
@@ -1,8 +1,8 @@
 class Hmap < Formula
   desc "Tool for working with Clang header map files"
   homepage "https://github.com/milend/hmap"
-  url "https://github.com/milend/hmap/archive/1.0.1.tar.gz"
-  sha256 "1a72c4d3f66d2e15fe78d3e252fa8a0b18d2f86c20f4e48383d8f1698ae98c99"
+  url "https://github.com/milend/hmap/archive/1.0.2.tar.gz"
+  sha256 "b1759705ad77405019034c0d44f6d9a3f43586da39f381fb701b5df5df957f08"
 
   head "https://github.com/milend/hmap.git"
 
