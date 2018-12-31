0a97361dcdd7526939a3573276885f832354df90
Milen Dzhumerov <milen@thecosmicmachine.com>
2018-12-31 21:26:08 +0000

    Update to v1.0.3

--- Formula/hmap.rb
+++ Formula/hmap.rb
@@ -1,12 +1,12 @@
 class Hmap < Formula
   desc "Tool for working with Clang header map files"
   homepage "https://github.com/milend/hmap"
-  url "https://github.com/milend/hmap/archive/1.0.2.tar.gz"
-  sha256 "b1759705ad77405019034c0d44f6d9a3f43586da39f381fb701b5df5df957f08"
+  url "https://github.com/milend/hmap/archive/1.0.3.tar.gz"
+  sha256 "979dcf6a71c3760896c3de9e2058c179b7f5f8907fbf620c5198ecac237bca1d"
 
   head "https://github.com/milend/hmap.git"
 
-  depends_on :xcode => ["8.3", :build]
+  depends_on :xcode => ["10.0", :build]
 
   def install
     system "swift",
